d0489d96ec2ead546ed39c5ea430414bb2a29ca3
Sort weste <00123216@uca.edu.sv>
2019-09-16 11:31:43 -0600

    Spring Setup

---
+++ src/main/java/com/sort/capas/swvicaria/SwvicariaApplication.java
@@ -0,0 +1,13 @@
+package com.sort.capas.swvicaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwvicariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwvicariaApplication.class, args);
+    }
+
+}
