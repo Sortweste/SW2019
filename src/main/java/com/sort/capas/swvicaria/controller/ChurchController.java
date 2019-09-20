25e68bef0ae569db71f96b2e3304ce4d1ca4de4d
raulcampost <raulcampos066@gmail.com>
2019-09-20 17:17:30 -0600

    modified view and show list of churchs

--- src/main/java/com/sort/capas/swvicaria/controller/ChurchController.java
+++ src/main/java/com/sort/capas/swvicaria/controller/ChurchController.java
@@ -1,10 +1,15 @@
 package com.sort.capas.swvicaria.controller;
 
+import com.sort.capas.swvicaria.domain.Church;
 import com.sort.capas.swvicaria.service.IChurchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/Church")
@@ -14,7 +19,47 @@ public class ChurchController {
     private IChurchService churchService;
 
     @GetMapping("/")
-    public String showAll(){
+    public String showAll(Model model) {
+        /* Test */
+        List<Church> list = new ArrayList<>();
+        Church church = new Church();
+
+        //church
+        church.setImg("/img/hotels/d1.jpg");
+        church.setName("Judas");
+        church.setHistory("la historia es muy larga para que un mortal como yo la cuente");
+        church.setAddress("Santa Tecla");
+        list.add(church);
+
+        //church
+        church = new Church();
+        church.setImg("/img/hotels/d2.jpg");
+        church.setName("Mateo");
+        church.setHistory("Otra historia muy larga por lo cual se contara luego");
+        church.setAddress("San Salvador");
+        list.add(church);
+
+        //church
+        church = new Church();
+        church.setImg("/img/hotels/d3.jpg");
+        church.setName("Lucas");
+        church.setHistory("ya no se que mas poner jajaja ando sin inspiracion");
+        church.setAddress("Apopa");
+        list.add(church);
+
+        //church
+        church = new Church();
+        church.setImg("/img/hotels/d4.jpg");
+        church.setName("Apocalipsis");
+        church.setHistory("mueran pecadores jajajajaj XD");
+        church.setAddress("San Salvador");
+        list.add(church);
+
+        /* Test */
+
+
+
+        model.addAttribute("churchs",list);
         return "hotels";
     }
 
--- src/main/java/com/sort/capas/swvicaria/domain/Church.java
+++ src/main/java/com/sort/capas/swvicaria/domain/Church.java
@@ -1,6 +1,8 @@
 package com.sort.capas.swvicaria.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -14,15 +16,23 @@ public class Church {
     @Column(name = "p_id")
     private Long id;
 
+    @Setter
+    @Getter
     @Column(name = "p_name")
     private String name;
 
+    @Setter
+    @Getter
     @Column(name = "p_address")
     private String address;
 
+    @Setter
+    @Getter
     @Column(name = "p_history")
     private String history;
 
+    @Setter
+    @Getter
     @Column(name = "p_picture")
     private String img;
 
