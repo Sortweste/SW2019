28f0a22dca5c4a2cd48f31b717292cbd32e1b219
raulcampost <raulcampos066@gmail.com>
2019-09-23 22:48:06 -0600

    new controller ControllerTest and view church

---
+++ src/main/java/com/sort/capas/swvicaria/controller/ControllerTest.java
@@ -0,0 +1,60 @@
+package com.sort.capas.swvicaria.controller;
+
+import com.sort.capas.swvicaria.domain.Church;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/Test")
+public class ControllerTest {
+
+    @GetMapping("/")
+    public String showAll(Model model) {
+        /* Test */
+        List<Church> list = new ArrayList<>();
+        Church church = new Church();
+        Church single = new Church();
+
+        //church
+        church.setImg("/img/hotels/d1.jpg");
+        church.setName("Judas");
+        church.setHistory("la historia es muy larga para que un mortal como yo la cuente");
+        church.setAddress("Santa Tecla");
+        list.add(church);
+
+        //church
+        church = new Church();
+        church.setImg("/img/hotels/d2.jpg");
+        church.setName("Mateo");
+        church.setHistory("Otra historia muy larga por lo cual se contara luego");
+        church.setAddress("San Salvador");
+        list.add(church);
+
+        //church
+        church = new Church();
+        church.setImg("/img/hotels/d3.jpg");
+        church.setName("Lucas");
+        church.setHistory("ya no se que mas poner jajaja ando sin inspiracion");
+        church.setAddress("Apopa");
+        list.add(church);
+
+        //church
+        single.setImg("/img/hotels/d4.jpg");
+        single.setName("Apocalipsis");
+        single.setHistory("mueran pecadores jajajajaj XD");
+        single.setAddress("San Salvador");
+
+        /* Test */
+
+
+        model.addAttribute("ch",list);
+        model.addAttribute("one",single);
+        return "church";
+    }
+}
+
