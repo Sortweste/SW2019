ecae23a4a3267ef58696000cc6ce8095cd234e2c
maxisun <00354615@uca.edu.sv>
2019-09-25 14:16:46 -0600

    spring security

---
+++ src/main/java/com/sort/capas/swvicaria/configuration/SpringSecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.sort.capas.swvicaria.configuration;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/","/css/**","/js/**","/img/**").permitAll();
+    }
+
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception{
+
+        PasswordEncoder encoder = passwordEncoder();
+        UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+
+        //usuarios en memoria
+        builder.inMemoryAuthentication()
+                .withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
+                .withUser(users.username("maxisun").password("123456").roles("USER"))
+                ;
+
+    }
+}
