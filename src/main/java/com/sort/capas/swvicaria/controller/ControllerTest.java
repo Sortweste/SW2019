64fe00dc4bc6e3dc651820bd447859872248355c
raulcampost <raulcampos066@gmail.com>
2019-09-26 17:19:54 -0600

    deleted css and js files that are not beign used

--- src/main/java/com/sort/capas/swvicaria/controller/ControllerTest.java
+++ src/main/java/com/sort/capas/swvicaria/controller/ControllerTest.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/Test")
 public class ControllerTest {
 
