3f8b6cb37d4bdf6e521c23028d85e40f6a70a0b7
Sortweste <00123216@uca.edu.sv>
2019-09-29 11:48:24 -0600

    DownLoad File Error fixed!

--- src/main/java/com/sort/capas/swvicaria/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/sort/capas/swvicaria/configuration/SpringSecurityConfiguration.java
@@ -1,12 +1,14 @@
 package com.sort.capas.swvicaria.configuration;
 
 import com.sort.capas.swvicaria.service.JpaUserDetailsService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,6 +30,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
