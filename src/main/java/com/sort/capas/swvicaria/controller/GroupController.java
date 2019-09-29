aca0849785061d75f6179eb593455f8b88eb279f
00198216 <00198216@uca.edu.sv>
2019-09-29 16:26:27 -0600

    Add groups implemented

--- src/main/java/com/sort/capas/swvicaria/controller/GroupController.java
+++ src/main/java/com/sort/capas/swvicaria/controller/GroupController.java
@@ -2,15 +2,19 @@ package com.sort.capas.swvicaria.controller;
 
 import com.sort.capas.swvicaria.domain.Church;
 import com.sort.capas.swvicaria.domain.Group;
+import com.sort.capas.swvicaria.service.IGroupService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/Group")
 public class GroupController {
+
+    @Autowired
+    private IGroupService groupService;
 
 
     @GetMapping("/")
@@ -28,6 +32,12 @@ public class GroupController {
     public String add(Model model){
         model.addAttribute("group", new Group());
         return "GroupForm";
+    }
+
+    @PostMapping("/saveGroup")
+    public String saveChurch(@ModelAttribute("group") Group group, @RequestParam("id") long id){
+        groupService.save(group, id);
+        return "redirect:/";
     }
 
 }
