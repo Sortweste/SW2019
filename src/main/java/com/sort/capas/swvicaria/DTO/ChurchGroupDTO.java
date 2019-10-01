b51563fedc1126be700d93f927799c98e5257294
Sortweste <00123216@uca.edu.sv>
2019-10-01 12:18:43 -0600

    BD Server Connected!

--- src/main/java/com/sort/capas/swvicaria/DTO/ChurchGroupDTO.java
+++ src/main/java/com/sort/capas/swvicaria/DTO/ChurchGroupDTO.java
@@ -1,6 +1,8 @@
 package com.sort.capas.swvicaria.DTO;
 
 import com.sort.capas.swvicaria.domain.Church;
+import com.sort.capas.swvicaria.domain.Group;
+import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -8,52 +10,10 @@ import javax.validation.constraints.Size;
 import java.sql.Date;
 import java.util.List;
 
+@Data
 public class ChurchGroupDTO {
 
-    @NotNull
-    @Size(min = 0, max = 100, message = "El nombre debe ser entre 0 y 100 caracteres")
-    // @Pattern(regexp = "[^1234567890]", message = "El nombre contiene numeros")
-
-    private String name;
-
-
-    private String information;
-
-
-
-    private Date schedule;
-
+    private Group group;
     private List<Church> church;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getInformation() {
-        return information;
-    }
-
-    public void setInformation(String information) {
-        this.information = information;
-    }
-
-    public Date getSchedule() {
-        return schedule;
-    }
-
-    public void setSchedule(Date schedule) {
-        this.schedule = schedule;
-    }
-
-    public List<Church> getChurch() {
-        return church;
-    }
-
-    public void setChurch(List<Church> church) {
-        this.church = church;
-    }
 }
