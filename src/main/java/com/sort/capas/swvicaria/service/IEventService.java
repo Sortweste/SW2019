38ccaec0520a3113c1c2338a5d6dc3a812103dfa
raulcampost <raulcampos066@gmail.com>
2019-11-07 15:04:34 -0600

    adding changes in .travis.yml

--- src/main/java/com/sort/capas/swvicaria/service/IEventService.java
+++ src/main/java/com/sort/capas/swvicaria/service/IEventService.java
@@ -1,13 +1,11 @@
 package com.sort.capas.swvicaria.service;
 
-import com.sort.capas.swvicaria.DTO.FormDTO;
 import com.sort.capas.swvicaria.domain.Event;
 
 import java.text.ParseException;
 
 public interface IEventService {
 
-    // public Event save(FormDTO[] dto) throws ParseException;
     public Event save(Event e) throws ParseException;
 
 }
