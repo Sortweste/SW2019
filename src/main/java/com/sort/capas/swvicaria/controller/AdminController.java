0373b7022fbffd03118982c030dcb459e5bfbf92
Sortweste <00123216@uca.edu.sv>
2019-11-10 12:43:09 -0600

    "Ready To Test!"

--- src/main/java/com/sort/capas/swvicaria/controller/AdminController.java
+++ src/main/java/com/sort/capas/swvicaria/controller/AdminController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/Admin")
@@ -32,6 +34,12 @@ public class AdminController {
     @GetMapping("/Group")
     public String groupDashBoard(Model model){
         model.addAttribute("groups", iGroupService.findAll());
+        return "adminGroup";
+    }
+
+    @PostMapping("/Group/Members")
+    public String manageMembersByGroup(@RequestParam("id") Long id, Model model){
+        model.addAttribute("members", iGroupService.findGroup(id).getUsers());
         return "adminGroup";
     }
 
