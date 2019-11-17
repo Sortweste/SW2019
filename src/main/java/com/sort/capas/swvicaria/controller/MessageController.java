46044621eeca9fb4a80346febae7618866469c3c
00198216 <00198216@uca.edu.sv>
2019-11-17 01:05:34 -0600

    Added Message option and page for Leader Roll

---
+++ src/main/java/com/sort/capas/swvicaria/controller/MessageController.java
@@ -0,0 +1,16 @@
+package com.sort.capas.swvicaria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Message")
+public class MessageController {
+
+    @GetMapping("/List")
+    public String groupDashBoard(){
+        return "email";
+    }
+}
