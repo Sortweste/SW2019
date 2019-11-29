18934be433edefd43ea05034b246237a6c91ab62
Sortweste <00123216@uca.edu.sv>
2019-11-29 16:18:18 -0600

    "All Admin Test Passed!"

--- src/test/java/com/sort/capas/swvicaria/AdminControllerIntegrationTest.java
+++ src/test/java/com/sort/capas/swvicaria/AdminControllerIntegrationTest.java
@@ -4,6 +4,7 @@ package com.sort.capas.swvicaria;
 import com.sort.capas.swvicaria.configuration.CustomLoginSuccessHandler;
 import com.sort.capas.swvicaria.controller.AdminController;
 import com.sort.capas.swvicaria.domain.Church;
+import com.sort.capas.swvicaria.domain.Event;
 import com.sort.capas.swvicaria.domain.Group;
 import com.sort.capas.swvicaria.service.IChurchService;
 import com.sort.capas.swvicaria.service.IEventService;
@@ -15,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,6 +27,7 @@ import java.util.List;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -55,12 +58,18 @@ public class AdminControllerIntegrationTest {
     public void setUp(){
         List<Church> churches = new ArrayList<>();
         List<Group> groups = new ArrayList<>();
+        List<Event> events = new ArrayList<>();
 
         Church church1 = new Church();
         Church church2 = new Church();
 
         Group group1 = new Group();
+        group1.setId((long)1);
+
         Group group2 = new Group();
+
+        Event event1 = new Event();
+        Event event2 = new Event();
 
         churches.add(church1);
         churches.add(church2);
@@ -68,8 +77,13 @@ public class AdminControllerIntegrationTest {
         groups.add(group1);
         groups.add(group2);
 
+        events.add(event1);
+        events.add(event2);
+
         when(iChurchService.findAll()).thenReturn(churches);
         when(iGroupService.findAll()).thenReturn(groups);
+        when(iEventService.findAll()).thenReturn(events);
+        when(iGroupService.findGroup((long) 1)).thenReturn(group1);
     }
 
     @Test
@@ -98,6 +112,34 @@ public class AdminControllerIntegrationTest {
                 .andExpect(status().isOk());
 
         verify(iGroupService, times(1)).findAll();
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"LIDER"})
+    public void whenEventDashBoard_thenReturnStatus() throws Exception {
+        mockMvc.perform(
+                get("/Admin/Event")
+
+        )
+                .andDo(print())
+                .andExpect(model().attribute("events",hasSize(2)))
+                .andExpect(status().isOk());
+
+        verify(iEventService, times(1)).findAll();
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"LIDER"})
+    public void whenManageMembersByGroup_thenReturnStatus() throws Exception {
+        mockMvc.perform(
+                post("/Admin/Group/Members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("id", String.valueOf(1))
+        )
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(iGroupService, times(1)).findGroup((long)1);
     }
 
 }
